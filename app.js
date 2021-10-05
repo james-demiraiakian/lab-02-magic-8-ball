9681790998194706b741da47d8fff092959482c6
James Demiraiakian <james.armen@gmail.com>
2021-10-05 16:24:58 -0700

    greyed out ask button after press, added reset button to try to rehide answer message when press. having issue with message reappearing immediately upon 2nd+ click of ask button

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ import { answers } from './answers.js';
 
 const button = document.getElementById('ask');
 const answer = document.getElementById('answer');
-//const question = document.getElementById('question');
+const question = document.getElementById('question');
 const ballShake = document.getElementById('ball-shake');
+const reset = document.getElementById('reset');
 
 button.addEventListener('click', () => {
     ballShake.play();
+    button.disabled = true;
     const randomIdx = Math.floor(Math.random() * answers.length);
     answer.textContent = answers[randomIdx];
     document.getElementById('ans-box').classList.remove('shake');
     setInterval(()=> {
         document.getElementById('ans-box').classList.add('shake');
     }, 1);
+    setInterval(()=> {
+        document.getElementById('answer').classList.remove('hidden');
+    }, 1500);
 });
 
+reset.addEventListener('click', () => {
+    document.getElementById('answer').classList.add('hidden');
+    answer.textContent = null;
+    button.disabled = false;
+});
 
 // if (question.textContent === '') {
 //     answer.textContent = 'Please ask a question';
