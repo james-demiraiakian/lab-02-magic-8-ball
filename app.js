b0667313866543c9df5d8799e11050dd470ab069
James Demiraiakian <james.armen@gmail.com>
2021-10-05 16:01:33 -0700

    misc styling/CSS, Added shake animation and sound to event listener

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ import { answers } from './answers.js';
 const button = document.getElementById('ask');
 const answer = document.getElementById('answer');
 //const question = document.getElementById('question');
-
+const ballShake = document.getElementById('ball-shake');
 
 button.addEventListener('click', () => {
+    ballShake.play();
     const randomIdx = Math.floor(Math.random() * answers.length);
     answer.textContent = answers[randomIdx];
+    document.getElementById('ans-box').classList.remove('shake');
+    setInterval(()=> {
+        document.getElementById('ans-box').classList.add('shake');
+    }, 1);
 });
 
 
