25d2db902dd51a0f6d782cbd8f29bc11a0c604b6
James Demiraiakian <james.armen@gmail.com>
2021-10-05 13:46:19 -0700

    wrote JS code and self tested. Properly outputs random index and displays answer contained within said index

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// import functions and grab DOM elements
-const colors = ['purple', 'green', 'black'];
-// initialize global state
+import { answers } from './answers.js';
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-const getRandomNumber = (max)=>{
-    return Math.floor(Math.random() * max);
-};
+const button = document.getElementById('ask');
+const answer = document.getElementById('answer');
 
-const randomIdx = Math.floor(Math.random() * colors.length);
-console.log(colors[randomIdx]);
-console.log(randomIdx);
+
+button.addEventListener('click', () => {
+    const randomIdx = Math.floor(Math.random() * answers.length);
+    answer.textContent = answers[randomIdx];
+});
