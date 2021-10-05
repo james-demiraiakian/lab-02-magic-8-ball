4c6e7378cb99b6d5fa824be4799624daf7368d09
James Demiraiakian <james.armen@gmail.com>
2021-10-05 13:15:44 -0700

    Basic HTML skeleton and simple CSS added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
-
+const colors = ['purple', 'green', 'black'];
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const getRandomNumber = (max)=>{
+    return Math.floor(Math.random() * max);
+};
+
+const randomIdx = Math.floor(Math.random() * colors.length);
+console.log(colors[randomIdx]);
+console.log(randomIdx);
