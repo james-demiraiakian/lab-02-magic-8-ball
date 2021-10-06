986515dcf0344c4b10189ade7c580a88ab36826d
James Demiraiakian <james.armen@gmail.com>
2021-10-05 17:38:07 -0700

    removed 'reset' button, now that I was able to achieve desired functionality. Also removed button disable on ask button for same reason

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ import { answers } from './answers.js';
 
 const button = document.getElementById('ask');
 const answer = document.getElementById('answer');
-//const question = document.getElementById('question');
 const ballShake = document.getElementById('ball-shake');
-const reset = document.getElementById('reset');
 
 const getRandomNumber = () => {
     return Math.floor(Math.random() * answers.length);
@@ -13,7 +11,6 @@ const getRandomNumber = () => {
 button.addEventListener('click', () => {
     answer.classList.add('hidden');
     ballShake.play();
-    //button.disabled = true;
     const randomIdx = getRandomNumber();
     answer.textContent = answers[randomIdx];
     document.getElementById('ans-box').classList.remove('shake');
@@ -23,9 +20,4 @@ button.addEventListener('click', () => {
     setTimeout(()=> {
         document.getElementById('answer').classList.remove('hidden');
     }, 1600);
-});
-
-reset.addEventListener('click', () => {
-    answer.classList.add('hidden');
-    button.disabled = false;
 });
